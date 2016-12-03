7a08f1173ef21d83ee7885c67435e66426f44be2
Andre Tost <andretost@us.ibm.com>
2016-12-03 15:30:19 -0600

    Another change

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
